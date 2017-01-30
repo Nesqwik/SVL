0b92fb7e4a32742064ca9a0dafbf2c5240c9d49d
Guilbert Louis <louis.guilbert@etudiant.univ-lille1.fr>
2017-01-30 12:17:44 +0100

    tp3

--- TP3/carte.py
+++ TP3/carte.py
@@ -1,4 +1,14 @@
-class Compte:
-    
-    def __init__(self):
-        self.balance = 0
+class Carte:
+    def __init__(self, solde, nb_ticket, prix_ticket):
+        self.m_solde = solde
+        self.m_nb_tickets = nb_ticket
+        self.m_prix_ticket = prix_ticket
+
+    def nb_ticket(self):
+        return self.m_nb_tickets
+
+    def solde(self):
+        return self.m_solde
+
+    def prix_ticket(self):
+        return self.m_prix_ticket
--- TP3/test_carte.py
+++ TP3/test_carte.py
@@ -1,26 +1,11 @@
-import carte
-from carte import Carte
-
 import unittest
 
-class TestCredit(unittest.Case):
-    def test_credit(selft):
-        c = Carte()
-        c.credit(10)
-        self.assertEqual(10,c.get_balance())
+import carte
+from carte import *
 
-    def test_credit_multiple(self):
-        c = Carte()
-        c.credit(21)
-        c.credit(21)
-        self.assertEqual(42,c.get_balance())
-    
-    def test_credit_negatif(self):
-        c = Carte()
-        with self.assertRaise(ValueError):
-            c.credit(-10)
-
-    def test_credit_zero(self):
-        c=Carte()
-        with self.assertRaise(ValueError):
-            c.credit(0)
+class TestCreationCarteAvecSoldeEtTicket(unittest.TestCase):
+    def test_creer_carte_avec_solde_et_ticket(self):
+        c = Carte(42, 42, 7)
+        self.assertEqual(42, c.nb_ticket())
+        self.assertEqual(42, c.solde())
+        self.assertEqual(7, c.prix_ticket())
